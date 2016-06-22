dff240015180eea92cb40c141ac77050c668e946
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-06-22 22:16:48 +0200

    Use Html.App.beginnerProgram to enhance Main.elm

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,5 @@
 import Html exposing (Html, text)
+import Html.App exposing (beginnerProgram)
 import Levenshtein exposing (levenshtein)
 
 u : String
@@ -7,4 +8,49 @@ u = "kangaroo"
 v : String
 v = "koala"
 
-main = text <| toString <| levenshtein u v
+main : Program Never
+main =
+  beginnerProgram
+  {
+    model = model
+  , update = update
+  , view = view
+  }
+
+
+-- MODEL
+
+
+type alias Model =
+  {
+    target : String
+  , current : String
+  }
+
+
+model : Model
+model =
+  {
+    target = "Hello, World!"
+  , current = ""
+  }
+
+
+-- UPDATE
+
+
+type Message =
+  DoNothing
+
+
+update : Message -> Model -> Model
+update message model =
+  model
+
+
+-- VIEW
+
+
+view : Model -> Html Message
+view model =
+  text <| toString <| levenshtein model.target model.current
